2fc97757bdebac81cddd7f467420f2846b1059a0
timmatsam <timmatsam@gmail.com>
2022-04-07 10:55:46 -0400

    build: cmnt out express server until it is needed

--- server.js
+++ server.js
@@ -1,26 +1,21 @@
-const express = require("express");
-const app = express();
-const path = require("path");
-// const PORT = process.env.PORT || 8000;
+// const express = require('express');
+// const path = require('path');
 
-//Heroku Step 4
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 8080;
-}
-app.listen(port, () => {
-  console.log(`Portfolio Website started on Port ${port}`);
-});
+// const app = express();
 
-app.use(express.static("public"));
-// app.get("/*", (req, res) => res.send(path("./public/index.html")));
-app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname, "public/index.html"), function (err) {
-    if (err) {
-      res.status(500).send(err);
-    }
-  });
-});
-// app.listen(PORT, function () {
-// 	console.log("Server started!");
+// let port = process.env.PORT;
+// if (port == null || port == '') {
+//   port = 8080;
+// }
+// app.listen(port, () => {
+//   console.log(`Portfolio Website started on Port ${port}`);
+// });
+
+// app.use(express.static('public'));
+// app.get('/*', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'public/index.html'), (err) => {
+//     if (err) {
+//       res.status(500).send(err);
+//     }
+//   });
 // });
