19c9e3a3423f1a8d8dfb8cd7101925b9aec2e28b
Tim Samuel <timmatsam@gmail.com>
2022-04-07 20:50:46 -0400

    Minor refactor (#2)
    * refactor: change structure of client dir
    
    * chore: remove unused components
    
    * build: change entry point
    
    * docs: update readme
    
    * refactor: change default to named exports
    
    * refactor: add eslinter (airbnb)
    
    * refactor: pass eslint checks

---
+++ client/components/solarsystem/threeJSmods/particles.js
@@ -0,0 +1,21 @@
+/* eslint-disable */
+const arrOfParticles = [];
+const particleCount = 1800;
+const pMaterial = new THREE.PointsMaterial({
+  size: 1,
+  blending: THREE.AdditiveBlending,
+  transparent: true,
+  map: pTexture,
+  color: 0x319bd3,
+});
+for (let i = 0; i < particleCount; i++) {
+  // let pY = 0 - (Math.random() * 1 - .5)
+  const pX = Math.random() * 200 - 100;
+  const pY = Math.random() * 100 - 50;
+  const pZ = Math.random() * 200 - 100;
+  const particle = new THREE.Vector3(pX, pY, pZ); // represents a point in space
+  arrOfParticles.push(particle);
+}
+const geometryP = new THREE.BufferGeometry().setFromPoints(arrOfParticles);
+const particleSystem = new THREE.Points(geometryP, pMaterial);
+scene.add(particleSystem);
