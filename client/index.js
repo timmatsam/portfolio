3befc67be6ab3b49b800b02dd4245f892b2171f5
timmatsam <timmatsam@gmail.com>
2022-04-23 11:01:29 -0400

    refactor: create component footer and archive

--- client/index.js
+++ client/index.js
@@ -2,9 +2,9 @@ import SolarSystem from './views/SolarSystem';
 import HeaderNav from './components/HeaderNav';
 import Homepage from './views/Homepage';
 import Projects from './views/Projects';
-import FooterNav from './components/FooterNav';
+import Footer from './components/Footer';
 import AboutMe from './views/AboutMe';
 
 export {
-  SolarSystem, HeaderNav, Homepage, Projects, FooterNav, AboutMe,
+  SolarSystem, HeaderNav, Homepage, Projects, Footer, AboutMe,
 };
