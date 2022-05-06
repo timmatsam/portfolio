e36197bb7187894933f4594de33f4516127d316c
timmatsam <timmatsam@gmail.com>
2022-05-05 23:00:54 -0400

    chore: pass eslint check

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   // eslint-disable-next-line no-console
   console.log(`Portfolio Website started on Port ${port}`);
